d996a1f5b8df5c2585337974403f77e6486dd57f
Alex <alex-postkasten@web.de>
2015-06-18 11:06:56 +0200

    artikel.java added

---
+++ src/Artikel.java
@@ -0,0 +1,21 @@
+public class Artikel
+{
+	private String artikelBezeichnung;
+	private float einzelPreis; //BigDecimal ...
+
+	public Artikel(String bezeichnung, float preis)
+	{
+		this.artikelBezeichnung = bezeichnung;
+		this.einzelPreis = preis;
+	}
+
+	public float getPreis()
+	{
+		return this.preis:
+	}
+	
+	public String getBezeichnung()
+	{
+		return this.artikelBezeichnung:
+	}
+}

59e678725922f21c11f5ef5c2400c1be3f3cc683
Alex <alex-postkasten@web.de>
2015-06-18 11:09:43 +0200

    Rechnung.java added

---
+++ src/Rechnung.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Rechnung
+{
+	private ArrayList<Artikel> artikel = new ArrayList<>();
+	private Printer printer;
+
+	public void add(Artikel artikel)
+	{
+		artikel.add(artikel)
+	}
+
+	public drucken()
+	{
+		printer.drucken();
+	}
+
+	setPrinter(Printer printer)
+	{
+		this.printer = printer;
+	}
+}
